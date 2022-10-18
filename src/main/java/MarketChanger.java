522b9109a09f57158c7945bc71af515f8bf04c97
K1rush4 <GomzikovKD@mail.ru>
2022-10-18 10:43:57 +0500

    second changes

--- src/main/java/MarketChanger.java
+++ src/main/java/MarketChanger.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.text.DecimalFormat;
 
 public class MarketChanger {
@@ -47,49 +46,3 @@ public class MarketChanger {
         return str;
     }
 }
-=======
-public class MarketChanger {
-
-    public static String MarketCacl(String StrSteamRub) {
-
-        String str;
-
-        try {
-            int IntSteamRub = Integer.parseInt(StrSteamRub);
-
-            if (IntSteamRub > 0) {
-                double resoult = IntSteamRub / 1.15;
-                str = Double.toString(resoult);
-            } else {
-                str = "Неверный формат данных";
-            }
-        } catch (Exception ex) {
-            str = "Неверный формат данных";
-        }
-
-        return str;
-    }
-
-
-    public static String MarketKPD(String StrMarketRub, String StrSteamRub) {
-
-        String str;
-
-        try {
-            int IntMarketRub = Integer.parseInt(StrMarketRub);
-            int IntSteamRub = Integer.parseInt(StrSteamRub);
-
-            if (IntMarketRub > 0 && IntSteamRub > 0) {
-                double resoult = 100 * (IntSteamRub / 1.15) / IntMarketRub;
-                str = Double.toString(resoult);
-            } else {
-                str = "Неверный формат данных";
-            }
-        } catch (Exception ex) {
-            str = "Неверный формат данных";
-        }
-
-        return str;
-    }
-}
->>>>>>> a4e5711 (Add files via upload)
