1ef816708b677ea4c66d8df80fb86f5c72b53e05
K1rush4 <114585762+K1rush4@users.noreply.github.com>
2022-10-18 09:06:59 +0500

    Add round function

--- src/main/java/QiwiChanger.java
+++ src/main/java/QiwiChanger.java
@@ -1,19 +1,16 @@
-import java.text.DecimalFormat;
-
 public class QiwiChanger {
     final static double QiwiToSteam = 0.83;
 
     public static String main(String StrQiwiRub) {
 
         String str;
-        DecimalFormat df = new DecimalFormat("#.##");
 
         try {
             int IntQiwiRub = Integer.parseInt(StrQiwiRub);
 
             if (IntQiwiRub > 0) {
                 double resoult = IntQiwiRub * QiwiToSteam;
-                str = df.format(resoult);
+                str = Double.toString(resoult);
             }
             else{
                 str = "Неверный формат данных";
